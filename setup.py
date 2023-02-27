792848dd4c4f864c1214b311ea43ce1a80cffc09
Will Barton <william.barton@cfpb.gov>
2023-02-27 12:13:34 -0500

    Switch to `pyproject.toml`
    With [PEP 621 support added to setuptools](https://github.com/pypa/setuptools/pull/2970) last year, and with pip supporting editable installs as of 21.3, we can move to `pyproject.toml` and deprecate `setup.py`. The file remains for legacy pip support.
    
    Because [flake8 does not want to support pyproject.tml yet](https://github.com/PyCQA/flake8/issues/234), unlike all the other tools we use, I’m removing it in favor of [ruff](https://github.com/charliermarsh/ruff).
    
    This change also continues support for Wagtail 3.
    
    Co-authored-by: Andy Chosak <andy.chosak@cfpb.gov>

--- setup.py
+++ setup.py
@@ -1,45 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
-install_requires = [
-    "tqdm>=4.15.0,<5",
-    "wagtail>=4.1",
-]
-
-
-testing_extras = [
-    "coverage>=3.7.0",
-    "mock>=1.0.0",
-]
-
-setup(
-    name="wagtail-inventory",
-    url="https://github.com/cfpb/wagtail-inventory",
-    author="CFPB",
-    author_email="tech@cfpb.gov",
-    description="Lookup Wagtail pages by block content",
-    long_description=open("README.rst", "r", encoding="utf-8").read(),
-    license="CCO",
-    version="1.6",
-    include_package_data=True,
-    packages=find_packages(),
-    python_requires=">=3.8",
-    install_requires=install_requires,
-    extras_require={"testing": testing_extras},
-    classifiers=[
-        "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.0",
-        "Framework :: Django :: 4.1",
-        "Framework :: Wagtail",
-        "Framework :: Wagtail :: 4",
-        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-        "License :: Public Domain",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-)
+setup()
