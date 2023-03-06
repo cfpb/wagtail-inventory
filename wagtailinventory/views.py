9c8cbc9f864e31d553c4fa6a677f138912a95ed7
Will Barton <william.barton@cfpb.gov>
2023-03-06 15:20:42 -0500

    Remove custom templates

--- wagtailinventory/views.py
+++ wagtailinventory/views.py
@@ -55,7 +55,7 @@ class BlockInventoryFilterSet(WagtailFilterSet):
 
 
 class BlockInventoryReportView(PageReportView):
-    template_name = "wagtailinventory/inventory_report.html"
+    template_name = "wagtailadmin/reports/base_page_report.html"
     title = "Block inventory"
     header_icon = "placeholder"
     filterset_class = BlockInventoryFilterSet
