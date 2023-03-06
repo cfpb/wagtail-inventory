bcc745cde9e0bc07279698d9a5c3e43344b9346d
Will Barton <william.barton@cfpb.gov>
2023-03-06 15:18:24 -0500

    Raise single warning to users without DAL

--- wagtailinventory/checks.py
+++ wagtailinventory/checks.py
@@ -2,42 +2,16 @@ from django.conf import settings
 from django.core.checks import Info, Tags, Warning, register
 
 
-def dal_present_check():
-    """Check that the django-autocomplete-light package is installed"""
+def dal_select2_check_all():
+    """Ensure django-autocomplete-light is fully installed"""
     try:
         from dal import autocomplete  # noqa: F401
     except (ModuleNotFoundError, ImportError):
         return False
 
-    return True
-
-
-def dal_installed_check():
-    """Check that the dal Django app is in INSTALLED_APPS"""
-    try:
-        assert "dal" in settings.INSTALLED_APPS
-    except AssertionError:
-        return False
-
-    return True
-
-
-def dal_select2_installed_check():
-    """Check that the dal_select2 Django app is in INSTALLED_APPS"""
-    try:
-        assert "dal_select2" in settings.INSTALLED_APPS
-    except AssertionError:
-        return False
-
-    return True
-
-
-def dal_select2_check_all():
-    """Ensure django-autocomplete-light is usable with all the above"""
     return (
-        dal_present_check()
-        and dal_installed_check()
-        and dal_select2_installed_check()
+        "dal" in settings.INSTALLED_APPS and
+        "dal_select2" in settings.INSTALLED_APPS
     )
 
 
@@ -45,34 +19,7 @@ def dal_select2_check_all():
 def dal_select2_system_check(app_configs, **kwargs):
     errors = []
 
-    if not dal_present_check():
-        errors.append(
-            Info(
-                "Could not import django-autocomplete-light autocomplete",
-                hint="Is the package installed?",
-                id="wagtailinventory.I001",
-            )
-        )
-
-    if not dal_installed_check():
-        errors.append(
-            Info(
-                "django-autocomplete-light is not in INSTALLED_APPS",
-                hint="Add 'dal' to INSTALLED_APPS",
-                id="wagtailinventory.I002",
-            )
-        )
-
-    if not dal_select2_installed_check():
-        errors.append(
-            Info(
-                "django-autocomplete-light's Select2 is not in INSTALLED_APPS",
-                hint="Add 'dal_select2' to INSTALLED_APPS",
-                id="wagtailinventory.I003",
-            )
-        )
-
-    if len(errors) > 0:
+    if not dal_select2_check_all():
         errors.append(
             Warning(
                 (
@@ -82,7 +29,7 @@ def dal_select2_system_check(app_configs, **kwargs):
                 hint=(
                     "Install django-autocomplete-light and add 'dal' and"
                     "'dal_select2' to INSTALLED_APPS. "
-                    "Add these check results to SILENCED_SYSTEM_CHECKS if "
+                    "Add this check result to SILENCED_SYSTEM_CHECKS if "
                     "you do not wish to use django-autocomplete-light."
                 ),
                 id="wagtailinventory.W001",
--- wagtailinventory/tests/test_checks.py
+++ wagtailinventory/tests/test_checks.py
@@ -5,57 +5,37 @@ from django.core.checks import Info, Warning
 from django.test import TestCase, override_settings
 
 from wagtailinventory.checks import (
-    dal_installed_check,
-    dal_present_check,
-    dal_select2_installed_check,
+    dal_select2_check_all,
     dal_select2_system_check,
 )
 
 
 class WagtailInventoryChecksTestCase(TestCase):
+
     @mock.patch.dict("sys.modules", {"dal": None})
-    def test_dal_present_check_fails(self):
-        self.assertFalse(dal_present_check())
+    def test_dal_select2_check_all_fails_on_import(self):
+        self.assertFalse(dal_select2_check_all())
 
-    def test_dal_present_check_passes(self):
-        self.assertTrue(dal_present_check())
+    @override_settings(INSTALLED_APPS=["dal_select2", ])
+    def test_dal_select2_check_all_fails_on_dal_installed(self):
+        self.assertFalse(dal_select2_check_all())
 
-    @override_settings(INSTALLED_APPS=[])
-    def test_dal_installed_check_fails(self):
-        self.assertFalse(dal_installed_check())
-
-    def test_dal_installed_check_passes(self):
-        self.assertTrue(dal_installed_check())
-
-    @override_settings(INSTALLED_APPS=[])
-    def test_dal_select2_installed_check_fails(self):
-        self.assertFalse(dal_select2_installed_check())
-
-    def test_dal_select2_installed_check_passes(self):
-        self.assertTrue(dal_select2_installed_check())
-
-    @override_settings(INSTALLED_APPS=[])
-    def test_dal_select2_check_all_fails(self):
-        self.assertFalse(dal_select2_installed_check())
+    @override_settings(INSTALLED_APPS=["dal", ])
+    def test_dal_select2_check_all_fails_on_select2_installed(self):
+        self.assertFalse(dal_select2_check_all())
 
     def test_dal_select2_check_all_passes(self):
-        self.assertTrue(dal_select2_installed_check())
+        self.assertTrue(dal_select2_check_all())
 
     def test_dal_select2_system_check_fails(self):
         with mock.patch.dict("sys.modules", {"dal": None}):
             errors = dal_select2_system_check(apps.get_app_configs())
-            self.assertEqual(len(errors), 2)
-            self.assertIsInstance(errors[0], Info)
-            self.assertIsInstance(errors[1], Warning)
-            self.assertEqual(errors[0].id, "wagtailinventory.I001")
-            self.assertEqual(errors[1].id, "wagtailinventory.W001")
+            self.assertEqual(len(errors), 1)
+            self.assertIsInstance(errors[0], Warning)
+            self.assertEqual(errors[0].id, "wagtailinventory.W001")
 
         with override_settings(INSTALLED_APPS=[]):
             errors = dal_select2_system_check(apps.get_app_configs())
-            self.assertEqual(len(errors), 3)
-            self.assertIsInstance(errors[0], Info)
-            self.assertIsInstance(errors[1], Info)
-            self.assertIsInstance(errors[2], Warning)
-            self.assertEqual(errors[0].id, "wagtailinventory.I002")
-            self.assertEqual(errors[1].id, "wagtailinventory.I003")
-            self.assertEqual(errors[2].id, "wagtailinventory.W001")
+            self.assertEqual(len(errors), 1)
+            self.assertIsInstance(errors[0], Warning)
+            self.assertEqual(errors[0].id, "wagtailinventory.W001")
